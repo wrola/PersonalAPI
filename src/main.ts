a825233646f05db413dbbf4dd6b63fab8dec4243
Wojciech Rola <wojciech.rola@start-up.house>
2023-11-18 10:59:41 +0100

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
+  app.use(helmet());
   await app.listen(port);
 }
 bootstrap();
