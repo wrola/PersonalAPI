ae3a3fa7225355773a781d084edfe15466409986
Wojciech Rola <wojciech.rola@start-up.house>
2023-11-29 18:27:31 +0100

    feat: add init host

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,7 +5,7 @@ import { Message } from './memory/core/entities/message.entity';
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
+  host: process.env.POSTGRES_HOST || 'postgres',
   port: 5432,
   username: process.env.POSTGRES_USER || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'test',
