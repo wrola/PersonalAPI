b174c3baf30711760e4f7def03a6662b201750dc
Wojciech Rola <w0j7455@gmail.com>
2024-03-05 15:46:14 +0100

    feat: remove console.log and make enviroment setup re-useable

---
+++ test/fixtures/enviroment.setup.ts
@@ -0,0 +1,18 @@
+import path = require('path');
+import { DockerComposeEnvironment, Wait } from 'testcontainers';
+
+export const environmentSetup = async () => {
+  const pathToCompose = path.resolve(__dirname, '../..') + '/';
+
+  return await new DockerComposeEnvironment(pathToCompose, 'compose.test.yaml')
+    .withWaitStrategy(
+      'qdrant_test',
+      Wait.forLogMessage('Qdrant HTTP listening on 6333', 1),
+    )
+    .withWaitStrategy(
+      'postgres_test',
+      Wait.forLogMessage('database system is ready to accept connections', 1),
+    )
+    .withBuild()
+    .up();
+};
